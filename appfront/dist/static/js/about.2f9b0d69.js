0aea13163f9b96b7223c4fb658808c4f6c6508ef
Glacierelk <2191710503@qq.com>
2023-06-04 16:39:16 +0800

    supplement

---
+++ appfront/dist/static/js/about.2f9b0d69.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkappfront"]=self["webpackChunkappfront"]||[]).push([[443],{5399:function(n,t,u){u.r(t),u.d(t,{default:function(){return f}});var s=function(){var n=this;n._self._c;return n._m(0)},a=[function(){var n=this,t=n._self._c;return t("div",{staticClass:"about"},[t("h1",[n._v("This is an about page")])])}],r=u(1001),e={},l=(0,r.Z)(e,s,a,!1,null,null,null),f=l.exports}}]);
+//# sourceMappingURL=about.2f9b0d69.js.map
